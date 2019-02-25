ca7ef5b9f1a7553c4a2bd1e2c36090d7a6436c95
Baoquan Dinh <baoquanxdinh@gmail.com>
2019-02-25 10:34:20 +0000

    Successfully added to aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@
 
   CarrierWave.configure do |config|
     config.storage    = :aws
-    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME') # for AWS-side bucket access permissions config, see section below
+    config.aws_bucket = ENV.fetch('AWS_BUCKET') # for AWS-side bucket access permissions config, see section below
     config.aws_acl    = 'private'
   
     # Optionally define an asset host for configurations that are fronted by a
@@ -19,8 +19,8 @@
     }
   
     config.aws_credentials = {
-      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      access_key_id:     ENV.fetch('AWS_ACCESS_KEY'),
+      secret_access_key: ENV.fetch('AWS_SECRET_KEY'),
       region:            ENV.fetch('AWS_REGION'), # Required
       stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
     }

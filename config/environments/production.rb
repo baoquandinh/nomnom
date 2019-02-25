3edd12277c6267cde67baa6b01870b2c27eba4e6
Baoquan Dinh <baoquanxdinh@gmail.com>
2019-02-25 21:06:16 +0000

    Correct URL for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.action_mailer.default_url_options = { host: 'https://nomnom-baoquan-dinh.herokuapp.com/' }
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

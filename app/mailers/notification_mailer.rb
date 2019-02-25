7ae2a75a7a95b0deb1330351aff3a321d3ee014e
Baoquan Dinh <baoquanxdinh@gmail.com>
2019-02-25 21:01:20 +0000

    Corrected method for sending email after comment

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,8 @@
 class NotificationMailer < ApplicationMailer
     default from: "no-reply@nomnom.com"
-    @place = comment.place
-    @place_owner = @place.user
     def comment_added(comment)
+        @place = comment.place
+        @place_owner = @place.user
         mail(to: @place_owner.email,
             subject: "A comment has been added to #{@place.name}")
     end
